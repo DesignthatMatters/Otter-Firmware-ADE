c60bbf249b41128441ff876079e4d2cefd5a23cb
kailevy <kai0levy@gmail.com>
2018-01-08 16:21:11 +0700

    Update alarm code to work with RGB leds

--- flashingAlarmTest/flashingAlarmTest/flashingAlarmTest.ino
+++ flashingAlarmTest/flashingAlarmTest/flashingAlarmTest.ino
@@ -1,38 +1,58 @@
-const int MEDIUM_LEDS = 11;
-const int HIGH_LEDS = 12;
+const int MEDIUM_LEDS = 10;
+const int HIGH_LEDS = 11;
+
+// change this for brightness
+const int BRITE = 255;
 
 const int HIGH_INTERVAL = freqToTime(2.0); // 2Hz flashing for high priority
 const int MEDIUM_INTERVAL = freqToTime(0.5);  // 0.5Hz flashing for medium priority
 
-int mediumState = 0; // State of the alarms -- should the be flashing
-int highState = 1;
+int mediumState = 1; // State of the alarms -- should the be flashing
+int highState = 0;
 
 int mediumLast = 0; // Last state of light of given alarm
 int highLast = 0;
 
 long mediumMillis = 0;
 long highMillis = 0;
-
+int readInt = 1;
 
 void setup() {
   pinMode(MEDIUM_LEDS, OUTPUT);
   pinMode(HIGH_LEDS, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  if (Serial.available() > 0) {    // This is to easily change the state of the medium alarm for testing
+    readInt = Serial.parseInt();
+    if (readInt == 2) {
+      mediumState = 1;
+      highState = 0;
+    } else if (readInt == 1) {
+      mediumState = 0;
+      highState = 0;
+    } else if (readInt == 3) {
+      highState = 1;
+      mediumState = 0;
+    }
+  }
+
   unsigned long currentMillis = millis();
   if (highState) {
+    mediumLast = 0;
+    // Use red to make red2
     if (toggleState(currentMillis, highMillis, HIGH_INTERVAL)) {
       highMillis = currentMillis;
       highLast = !highLast;
     }
   }
   else if (mediumState) {
-    // Only flash medium if high is not active
-    highLast = 0;
+    // Revision: use green and red to make yellow
     if (toggleState(currentMillis, mediumMillis, MEDIUM_INTERVAL)) {
       mediumMillis = currentMillis;
       mediumLast = !mediumLast;
+      highLast = mediumLast;
     }
   }
   else {
@@ -40,8 +60,9 @@ void loop() {
     mediumLast = 0;
   }
   
-  digitalWrite(HIGH_LEDS, highLast);
-  digitalWrite(MEDIUM_LEDS, mediumLast);
+  
+  analogWrite(HIGH_LEDS, highLast*BRITE);
+  analogWrite(MEDIUM_LEDS, mediumLast*BRITE);
   
 }
 
